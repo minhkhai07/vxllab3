871f1ed2ebbc2351029fb35ee688311661d7ee6c
ADMIN\Admin <khai.to0707@hcmut.edu.vn>
2024-10-21 13:45:58 +0700

    Init lab3

---
+++ lab3vxl/Core/Inc/Button.h
@@ -0,0 +1,20 @@
+/*
+ * Button.h
+ *
+ *  Created on: Oct 21, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+#include "main.h"
+
+#define NORMAL_STATE SET
+#define PRESSED_STATE RESET
+
+extern int button1_flag;
+
+void getKeyInput();
+
+
+#endif /* INC_BUTTON_H_ */
---
+++ lab3vxl/Core/Inc/Timer.h
@@ -0,0 +1,23 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Oct 21, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+#define MAX_TIMER	5
+
+extern int TIMER_CYCLE;
+
+extern int Timer_Counter[MAX_TIMER];
+extern int Timer_Flag[MAX_TIMER];
+
+void setTimer(int index, int duration);
+
+void runTimer(void);
+
+
+
+#endif /* INC_TIMER_H_ */
